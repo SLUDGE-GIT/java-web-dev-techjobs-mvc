7b5a4c205150b7ca381d659b7f75f59c55100036
Kyle <florez.photo@gmail.com>
2020-10-19 01:07:21 -0500

    looked at bonus - and tried doing it with my old table and reverted everything because it kept sending me error whitepages.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,7 +25,7 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-        @RequestMapping (value = "results", method = RequestMethod.POST)
+        @RequestMapping (value = "/results")
     public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
             if (searchTerm.equals("") || searchTerm.toLowerCase().equals("all")) {
